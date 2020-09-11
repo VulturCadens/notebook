e36d021aaeea73c527cb6a624b2e7ba9adcf1a19
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2020-09-11 18:31:10 +0300

    Functions to set, clear and test a bit.

--- BinaryOperators/main.go
+++ BinaryOperators/main.go
@@ -10,9 +10,23 @@ var (
 	word  uint16 = 0x22aa
 )
 
+func setBit(b, m byte) byte {
+	b |= m
+	return b
+}
+
+func clearBit(b, m byte) byte {
+	b &^= m
+	return b
+}
+
+func testBit(b, m byte) bool {
+	return (b & m) != 0
+}
+
 func main() {
 	/*
-	 * Decodes an Uint16 from a byte slice.
+	 * Decodes a Uint16 from a byte slice.
 	 */
 
 	wordLittleEndian := binary.LittleEndian.Uint16(bytes[0:])
@@ -43,5 +57,17 @@ func main() {
 
 	fmt.Printf(" 1100 AND \n 0101 -> \n %04b \n\n", AND)
 	fmt.Printf(" 1100 OR \n 0101 -> \n %04b \n\n", OR)
-	fmt.Printf(" 1100 XOR \n 0101 -> \n %04b \n", XOR)
+	fmt.Printf(" 1100 XOR \n 0101 -> \n %04b \n\n", XOR)
+
+	/*
+	 * Call functions to set, clear and test a bit.
+	 */
+
+	var b byte = 0b1000
+
+	fmt.Printf("SetBit    (Set first)    %04b -> %04b \n", b, setBit(b, 0b0001))
+	fmt.Printf("ClearBit  (Clear fourth) %04b -> %04b \n", b, clearBit(b, 0b1000))
+	fmt.Printf("TestBit   (Fourth set?)  %04b -> %t \n", b, testBit(b, 0b1000))
+	fmt.Printf("TestBit   (First set?)   %04b -> %t \n", b, testBit(b, 0b0001))
+
 }
