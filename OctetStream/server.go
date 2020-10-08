1b53ca507c2a957b9f69cc0d1edcd29b421ff39b
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2020-10-08 20:47:42 +0300

    Uint16 (ordering little-endian).

--- OctetStream/server.go
+++ OctetStream/server.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net/http"
 	"time"
 )
 
-var directory = http.Dir(".")
+var (
+	directory = http.Dir(".")
+	words     = [2]uint16{255, 65280}
+	twoBytes  = make([]byte, 2, 2)
+)
 
 func index(w http.ResponseWriter, r *http.Request) {
 	file, err := directory.Open("index.html")
@@ -24,7 +29,13 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func octet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Write([]byte{0xFF, 0x00, 0x00, 0xFF})
+
+	for _, word := range words {
+		binary.LittleEndian.PutUint16(twoBytes[0:], word)
+		w.Write(twoBytes)
+
+		fmt.Printf("Sent %d = %d (little-endian)\n", twoBytes, word)
+	}
 }
 
 func main() {
