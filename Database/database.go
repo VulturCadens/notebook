c2feda4071e7ba5e34dc84d5464a02722abcc13b
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2020-10-15 17:50:45 +0300

    Tweak a database.

--- Database/database.go
+++ Database/database.go
@@ -10,12 +10,29 @@ import (
 	_ "github.com/mattn/go-sqlite3" // https://github.com/mattn/go-sqlite3
 )
 
+type book struct {
+	author string
+	title  string
+	year   int
+}
+
+var books = []book{
+	book{"Victor Hugo", "Les Misérables", 1862},
+	book{"Victor Hugo", "The Hunchback of Notre-Dame", 1831},
+	book{"Nikolai Gogol", "Dead Souls", 1842},
+	book{"Fyodor Dostoevsky", "Crime and Punishment", 1866},
+	book{"Leo Tolstoy", "War and Peace", 1869},
+	book{"Leo Tolstoy", "Anna Karenina", 1877},
+	book{"Alexandre Dumas", "The Count of Monte Cristo", 1844},
+}
+
 func createTable(db *sql.DB) {
 	_, err := db.Exec(
 		`CREATE TABLE IF NOT EXISTS books (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			author TEXT,
 			title TEXT,
+			year INTEGER,
 			created DATETIME
 		);`)
 
@@ -24,7 +41,7 @@ func createTable(db *sql.DB) {
 	}
 }
 
-func countBooks(db *sql.DB) int {
+func countRows(db *sql.DB) int {
 	var count int
 
 	_ = db.QueryRow("SELECT COUNT(*) FROM books").Scan(&count)
@@ -32,22 +49,20 @@ func countBooks(db *sql.DB) int {
 	return count
 }
 
-func addBook(db *sql.DB, author, title string) error {
-	statement, err := db.Prepare("INSERT INTO books(author, title, created) VALUES(?, ?, ?)")
+func addRow(db *sql.DB, author, title string, year int) {
+	statement, err := db.Prepare("INSERT INTO books(author, title, year, created) VALUES(?, ?, ?, ?)")
 
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 
 	defer statement.Close()
 
-	_, err = statement.Exec(author, title, time.Now())
+	_, err = statement.Exec(author, title, year, time.Now())
 
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-
-	return nil
 }
 
 func main() {
@@ -65,9 +80,9 @@ func main() {
 
 	createTable(db)
 
-	if err := addBook(db, "Victor Hugo", "Les Misérables"); err != nil {
-		log.Fatal(err)
+	for _, book := range books {
+		addRow(db, book.author, book.title, book.year)
 	}
 
-	fmt.Printf("There is %d book(s). \n", countBooks(db))
+	fmt.Printf("There is %d rows in the database. \n", countRows(db))
 }
