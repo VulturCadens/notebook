f9fe8a0e2d32343cd38342d04be3e1f94e55eb90
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2020-11-20 19:54:47 +0200

    Add a service worker.

--- ProgressiveWebApplication/WWW/js/main.js
+++ ProgressiveWebApplication/WWW/js/main.js
@@ -1,3 +1,10 @@
 window.onload = () => {
-    console.log("OK")
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("/pwa/sw/service.js",{ scope: "/pwa/sw/" })
+            .then((registration) => {
+                console.log("Registration succeeded. Scope is " + registration.scope)
+            }).catch((error) => {
+                console.log("Registration failed with " + error)
+            })
+    }
 }
