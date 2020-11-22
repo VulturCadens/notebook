decebd10b8d1d1503f14f8f18768fee586ae0c02
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2020-11-22 08:42:52 +0200

    Install and Activate events.

--- ProgressiveWebApplication/WWW/pwa/sw/service.js
+++ ProgressiveWebApplication/WWW/pwa/sw/service.js
@@ -1,1 +1,7 @@
-console.log("Service worker OK.")
+self.addEventListener("install", (event) => {
+    console.log("Install event.")
+})
+
+self.addEventListener("activate", (event) => {
+    console.log("Activate event.")
+})
