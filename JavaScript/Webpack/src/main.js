36c9bcce611a78f85bc3c1d9603edb3f5c7faf95
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2020-11-30 19:53:19 +0200

    Add Vuejs example.

--- JavaScript/Webpack/src/main.js
+++ JavaScript/Webpack/src/main.js
@@ -1,3 +1,14 @@
-window.onload = () => {
-    console.log("OK")
+const CounterApp = {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    mounted() {
+        setInterval(() => {
+            this.counter++
+        }, 200)
+    }
 }
+
+Vue.createApp(CounterApp).mount("#counter")
