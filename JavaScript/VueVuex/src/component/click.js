6ab2bdb474d5f846165822179aec1fc44eb4239e
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2020-12-16 17:21:42 +0200

    Transitions classes.

--- JavaScript/VueVuex/src/component/click.js
+++ JavaScript/VueVuex/src/component/click.js
@@ -3,6 +3,12 @@ export default {
 
     template: `
         <div>
+            <div class="container">
+                <transition name="animation">
+                    <div class="box" v-show="show"></div>
+                </transition>
+            </div>
+            
             <h1>Click counter is {{ counter }}</h1>
             <button v-on:click="clicked">Click</button>
         </div>
@@ -10,12 +16,14 @@ export default {
 
     data() {
         return {
-            counter: 0
+            counter: 0,
+            show: true
         }
     },
 
     methods: {
         clicked() {
+            this.show = !this.show
             this.counter++
         }
     }
