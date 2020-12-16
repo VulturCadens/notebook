f40a49e48be709d855324efde39cf74987d69bac
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2020-12-16 16:15:08 +0200

    Add webpack-dev-server.

--- JavaScript/VueVuex/webpack.config.js
+++ JavaScript/VueVuex/webpack.config.js
@@ -6,5 +6,11 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "www/javascript")
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, "www"), // The root directory.
+    publicPath: "/javascript/",               // The base path where dev-server store the bundle in memory.
+    port: 8000
   }
 }
