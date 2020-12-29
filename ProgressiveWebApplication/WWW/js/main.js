1537cb4e5d411b9f11a0c1fb3f24c711428819db
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2020-12-29 18:43:58 +0200

    Fetch an image.

--- ProgressiveWebApplication/WWW/js/main.js
+++ ProgressiveWebApplication/WWW/js/main.js
@@ -1,10 +1,27 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/pwa/sw/service.js", { scope: "/pwa/sw/" })
+        .then((registration) => {
+            console.log("Registration succeeded. Scope is " + registration.scope)
+        }).catch((error) => {
+            console.log("Registration failed with " + error)
+        })
+}
+
 window.onload = () => {
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/pwa/sw/service.js",{ scope: "/pwa/sw/" })
-            .then((registration) => {
-                console.log("Registration succeeded. Scope is " + registration.scope)
-            }).catch((error) => {
-                console.log("Registration failed with " + error)
-            })
-    }
+    const imageElement = document.createElement("img")
+    const parentElement = document.getElementById("parent")
+
+    fetch("/image/picture.jpg")
+        .then(response => response.blob())
+
+        .then(blob => {
+            const imageURL = window.URL.createObjectURL(blob)
+
+            imageElement.src = imageURL
+            parentElement.appendChild(imageElement)
+        })
+
+        .catch((error) => {
+            console.log("Image failed with " + error)
+        })
 }
