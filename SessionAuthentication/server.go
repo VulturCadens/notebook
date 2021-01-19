21eb2aef03b0388b08b5651380e75254a82a43f9
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2021-01-19 09:42:30 +0200

    Add bcrypt and UUID.

--- SessionAuthentication/server.go
+++ SessionAuthentication/server.go
@@ -5,19 +5,24 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 const cookieName = "session_example"
-const cookieValue = "foobar"
+
+var cookieValue string
 
 var users = map[string]string{
-	"john": "smith",
+	"john": "$2a$04$zS2sTndQnwWe53Vy6eQ2NuwvL06sVGDcdwAaRdta4GPbBzE73ZZUK", // password 'smith'
 }
 
 func authentication(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		c, err := r.Cookie(cookieName)
+		cookie, err := r.Cookie(cookieName)
 
 		if err != nil {
 			if errors.Is(err, http.ErrNoCookie) {
@@ -29,19 +34,13 @@ func authentication(h http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
-		token := c.Value
-
-		if token != cookieValue {
+		if cookie.Value != cookieValue {
 			http.Error(w, "403", http.StatusForbidden)
 			return
 		}
 
 		h(w, r)
 	}
-}
-
-func void(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "404", http.StatusNotFound)
 }
 
 func welcome(w http.ResponseWriter, r *http.Request) {
@@ -59,31 +58,38 @@ func welcome(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if _, ok := users[username]; ok {
-		if users[username] == password {
-			file, err := os.Open("welcome.html")
+		err := bcrypt.CompareHashAndPassword([]byte(users[username]), []byte(password))
 
-			if err != nil {
-				http.Error(w, "500", http.StatusInternalServerError)
-				return
-			}
-
-			defer file.Close()
-
-			http.SetCookie(w, &http.Cookie{
-				Name:     cookieName,
-				Value:    cookieValue,
-				Path:     "/session",
-				HttpOnly: true,
-				SameSite: http.SameSiteStrictMode,
-				MaxAge:   120,
-				// Secure:   true,
-				// Expires:  time.Now().Add(120 * time.Second),
-			})
-
-			w.Header().Set("Content-Type", "text/html")
-			http.ServeContent(w, r, "", time.Now(), file)
+		if err != nil {
+			http.Error(w, "401", http.StatusUnauthorized)
 			return
 		}
+
+		file, err := os.Open("welcome.html")
+
+		if err != nil {
+			http.Error(w, "500", http.StatusInternalServerError)
+			return
+		}
+
+		defer file.Close()
+
+		cookieValue = uuid.New().String()
+
+		http.SetCookie(w, &http.Cookie{
+			Name:     cookieName,
+			Value:    cookieValue,
+			Path:     "/session",
+			HttpOnly: true,
+			SameSite: http.SameSiteStrictMode,
+			MaxAge:   120,
+			// Secure:   true,
+			// Expires:  time.Now().Add(120 * time.Second),
+		})
+
+		w.Header().Set("Content-Type", "text/html")
+		http.ServeContent(w, r, "", time.Now(), file)
+		return
 	}
 
 	http.Error(w, "401", http.StatusUnauthorized)
@@ -119,8 +125,19 @@ func application() http.HandlerFunc {
 	}
 }
 
+func staticFiles(w http.ResponseWriter, r *http.Request) {
+	p := r.URL.Path
+
+	if p == "/" {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	http.ServeFile(w, r, path.Join("./static", p))
+}
+
 func main() {
-	http.HandleFunc("/", void)
+	http.HandleFunc("/", staticFiles)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/session/welcome", welcome)
 	http.HandleFunc("/session/application", authentication(application()))
