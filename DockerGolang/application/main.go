741311c4fcf3585673ad392a66a0ea233f02b765
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2021-02-19 16:43:19 +0200

    Add logger and counter.

--- DockerGolang/application/main.go
+++ DockerGolang/application/main.go
@@ -1,10 +1,53 @@
 package main
 
 import (
+	"bufio"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"text/template"
 	"time"
 )
+
+var (
+	count     int
+	templates = template.Must(template.ParseFiles("./www/index.html"))
+)
+
+func init() {
+	file, err := os.Open("count.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	s := scanner.Text()
+
+	file.Close()
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err = os.Create("count.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	i++
+	s = strconv.Itoa(i)
+
+	if _, err := file.WriteString(s); err != nil {
+		log.Fatal(err)
+	}
+
+	count = i
+	log.Printf("[%d]\n", i)
+}
 
 func favicon(w http.ResponseWriter, r *http.Request) {
 	file, err := os.Open("./www/favicon-32x32.png")
@@ -21,17 +64,12 @@ func favicon(w http.ResponseWriter, r *http.Request) {
 }
 
 func response(w http.ResponseWriter, r *http.Request) {
-	file, err := os.Open("./www/index.html")
+	content := &struct{ Count int }{count}
 
-	if err != nil {
+	if err := templates.ExecuteTemplate(w, "INDEX", content); err != nil {
 		http.Error(w, "500", http.StatusInternalServerError)
 		return
 	}
-
-	defer file.Close()
-
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	http.ServeContent(w, r, "", time.Now(), file)
 }
 
 func main() {
