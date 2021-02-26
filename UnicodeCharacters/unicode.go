3636579d78a1526adf175d652d576ab3019d4c93
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2021-02-26 13:11:54 +0200

    Iterate over the bytes of a string.

--- UnicodeCharacters/unicode.go
+++ UnicodeCharacters/unicode.go
@@ -5,7 +5,7 @@ import (
 	"unicode/utf8"
 )
 
-var unicodeString string = "ⓈωA◆‰"
+var unicodeString string = "aωA◆"
 
 func main() {
 	/*
@@ -15,15 +15,20 @@ func main() {
 	fmt.Printf("There is %d runes in string (%s). \n", utf8.RuneCountInString(unicodeString), unicodeString)
 	fmt.Printf("Length of string is %d bytes. \n\n", len(unicodeString))
 
+	fmt.Println("Iterate over the characters of a string (the range expression).")
 	for i, r := range unicodeString {
-		fmt.Printf("Index: %d  Character: %c  Unicode: %d\n", i, r, r)
+		fmt.Printf("  Index: %d  Character: %c  Unicode: %d\n", i, r, r)
 	}
 
-	fmt.Println()
+	fmt.Println("\nIterate over the bytes of a string.")
+	for i, b := range []byte(unicodeString) {
+		fmt.Printf("  Index: %-2d  Byte: %d\n", i, b)
+	}
 
+	fmt.Println("\nIterate over the characters of a string (utf8.DecodeRuneInString).")
 	for i := 0; i < len(unicodeString); {
 		rune, size := utf8.DecodeRuneInString(unicodeString[i:])
-		fmt.Printf("Index: %d  Character: %c\n", i, rune)
+		fmt.Printf("  Index: %d  Size (bytes): %d  Character: %c\n", i, size, rune)
 		i += size
 	}
 }
