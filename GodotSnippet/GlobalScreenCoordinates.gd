e39f53e9af6e7c5356d43ceaf45b80b88c01042b
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2021-03-10 19:18:42 +0200

    Comment.

--- GodotSnippet/GlobalScreenCoordinates.gd
+++ GodotSnippet/GlobalScreenCoordinates.gd
@@ -1,9 +1,13 @@
 #
 # Get the global coordinates of the top of the screen.
 #
+# -- The viewport doesn't move with the camera. The global position of the viewport doesn't change.
+# -- The camera changes the transform of the viewport.
+# -- A return value (get_camera_screen_center) is relative to the world origin.
+#
 
 func get_camera_rect(camera : Camera2D) -> Rect2:
-	var cameraPos : Vector2 = camera.get_camera_screen_center() # Relative to the world origin.
+	var cameraPos : Vector2 = camera.get_camera_screen_center()
 	var viewportSize : Vector2 = get_viewport_rect().size * camera.zoom
 
 	var position : Vector2 = Vector2(cameraPos.x - (viewportSize.x / 2), cameraPos.y - (viewportSize.y / 2))
