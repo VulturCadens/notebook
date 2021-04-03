07a779cbdd9c541611bc95e542a8826111b2c4ee
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2021-04-03 03:10:02 +0300

    Multithreaded client.

--- UnixDomainSocket/client/client.py
+++ UnixDomainSocket/client/client.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 
-import sys
-import socket
 import os
+import sys
+import time
+import socket
+import threading
 
+from _thread import start_new_thread
+
+def read(connection):
+    while True:
+        message = connection.recv(128)
+        
+        if not message:
+            return
+
+        print("Client got: " + message.decode("utf-8"))
 
 def main() -> int:
     if os.path.exists("/tmp/vultur.sock"):
         try:
-            client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            client.connect("/tmp/vultur.sock")
+            connection = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            connection.connect("/tmp/vultur.sock")
 
         except ConnectionRefusedError:
             print("[ConnectionRefusedError]")
@@ -19,13 +31,15 @@ def main() -> int:
         print("[Path doesn't exist]")
         return 1
 
-    with client:
+    start_new_thread(read, (connection,))
+
+    with connection:
         while True:
             try:
                 message = input("Enter a string: ")
 
                 if message != "":
-                    client.send(message.encode("utf-8"))
+                    connection.send(message.encode("utf-8"))
 
             except BrokenPipeError:
                 print("[BrokenPipeError]")
@@ -34,6 +48,8 @@ def main() -> int:
             except KeyboardInterrupt:
                 print("\n[Ctrl-C]")
                 return 0
+
+            time.sleep(0.25)
 
 
 if __name__ == "__main__":
