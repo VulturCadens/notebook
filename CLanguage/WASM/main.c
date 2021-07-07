f769384bf59a3129158a2dcc64294a86ecac3569
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2021-07-07 19:32:46 +0300

    Minimal WebAssembly.

---
+++ CLanguage/WASM/main.c
@@ -0,0 +1,12 @@
+/*
+ * emcc -o index.html main.c -O3 -s WASM=1 --shell-file shell_minimal.html
+ * python3 -m http.server
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Minimal\n");
+    return 0;
+}
