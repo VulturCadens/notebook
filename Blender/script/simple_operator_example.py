83c0292da59360c1604ee65e09b3bebe8eb288cd
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2021-07-29 10:44:03 +0300

    Print the collection.

--- Blender/script/simple_operator_example.py
+++ Blender/script/simple_operator_example.py
@@ -4,22 +4,31 @@ import bpy
 # bpy.ops.object.simple_operator_example()
 #
 
+
 class OperatorExample(bpy.types.Operator):
     bl_idname = "object.simple_operator_example"
     bl_label = "Operator Example"
 
     def execute(self, context):
         print(self.bl_label)
-        return {'FINISHED'}
+
+        if not context.selected_objects:
+            print("The collection is empty")
+            return {"FINISHED"}
+
+        for obj in context.selected_objects:
+            print(obj.name)
+
+        return {"FINISHED"}
 
 
 def register():
     bpy.utils.register_class(OperatorExample)
+
 
 def unregister():
     bpy.utils.unregister_class(OperatorExample)
 
 
 if __name__ == "__main__":
-    register() 
-
+    register()
