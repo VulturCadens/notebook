90527bcee54c5c6227880ae2a4e787cc751e9e93
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2021-11-08 13:26:16 +0200

    Requests is a simple HTTP library for Python.

---
+++ Python/Requests/main.py
@@ -0,0 +1,27 @@
+# pylint: skip-file
+
+import sys
+import requests
+
+
+def main() -> int:
+    try:
+        r = requests.get("https://example.com")
+
+        if r.status_code == requests.codes.ok:
+            print("Encoding: " + r.encoding + "\n")
+
+            for h in r.headers:
+                print(h + ": " + r.headers[h])
+
+            return 0
+
+        print("ERROR: Status Code:", str(r.status_code))
+        return 1
+
+    except requests.ConnectionError as error:
+        print("ERROR:", error)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
