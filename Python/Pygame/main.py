b24ca5acc39677ee6bf95e13ee6cdea17d432cb3
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2022-02-17 19:02:22 +0200

    Add simple Pygame application.

---
+++ Python/Pygame/main.py
@@ -0,0 +1,61 @@
+import os
+import pygame as SDL
+
+FPS = 30
+
+DIRECTORY = os.path.split(os.path.abspath(__file__))[0]
+
+BOX_PATH = os.path.join(DIRECTORY, "images", "box.png")
+BACKGROUND_PATH = os.path.join(DIRECTORY, "images", "background.jpg")
+
+
+def main():
+    speed = 1
+    x = 100
+    clock = SDL.time.Clock()
+
+    SDL.init()
+
+    screen = SDL.display.set_mode((1000, 500))
+
+    box = SDL.image.load(BOX_PATH).convert()
+    background = SDL.image.load(BACKGROUND_PATH).convert()
+    window_icon = SDL.image.load("icon.png")  # 32 x 32 pixels
+
+    SDL.display.set_icon(window_icon)
+    SDL.display.set_caption("Window Title")
+
+    while True:
+        for event in SDL.event.get():
+            if event.type in (SDL.QUIT, SDL.KEYDOWN):
+                return
+
+            elif event.type == SDL.MOUSEBUTTONDOWN:
+                left_button, middle_button, right_button = SDL.mouse.get_pressed()
+
+                if left_button:
+                    position = SDL.mouse.get_pos()
+                    print("Left mouse button event: {}".format(position))
+
+                elif middle_button:
+                    print("Middle mouse button event")
+
+                elif right_button:
+                    print("Right mouse button event")
+
+        screen.blit(background, (0, 0))
+        screen.blit(box, (x, 100))
+
+        x += speed
+
+        if x >= 900 or x <= 10:
+            speed *= -1
+
+        SDL.display.update()
+
+        clock.tick(FPS)
+
+
+if __name__ == "__main__":
+    main()
+    SDL.quit()
