ac7fdb67de46078a84c4361562221e12bfb99fbd
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2023-09-01 11:06:33 +0300

    Add module example.

---
+++ Miscellaneous/SimpleModule/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"examplemodule/submodule"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(submodule.Greeting())
+}
---
+++ Miscellaneous/SimpleModule/submodule/golangfile.go
@@ -0,0 +1,5 @@
+package submodule
+
+func Greeting() string {
+	return "Hello"
+}
