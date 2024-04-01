bc52e05594f84ed2aaa78a91983cda660d20c366
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2024-04-01 21:40:01 +0300

    Spaces -> Tabs.

--- JavaScript/Electron/main.js
+++ JavaScript/Electron/main.js
@@ -6,9 +6,9 @@ const nodejsCrypto = require("node:crypto")
 const createWindow = () => {
 	const win = new BrowserWindow({
 		width: 960,
-        height: 540,
-        x: 100,
-        y: 100,
+		height: 540,
+		x: 100,
+		y: 100,
 		icon: "./assets/favicon-32x32.png",
 		webPreferences: {
 			preload: path.join(__dirname, "preload.js")
