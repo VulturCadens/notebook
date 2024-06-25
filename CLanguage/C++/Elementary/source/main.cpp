5f2343f6ceeff71570b8e5d1f74100c833f4266e
VulturCadens <69871388+VulturCadens@users.noreply.github.com>
2024-06-25 19:03:37 +0300

    Add an inline SVG and mouse events.

--- CLanguage/C++/Elementary/source/main.cpp
+++ CLanguage/C++/Elementary/source/main.cpp
@@ -35,6 +35,18 @@ int main()
 	SDL_SetWindowIcon(window, iconSurface);
 	SDL_FreeSurface(iconSurface);
 
+	/* Create an SDL surface using an SVG from an string. */
+
+	const std::string svgString =
+		"<svg height='100' width='100'>"
+		"<circle cx='50' cy='50' r='45' stroke='#202020' stroke-width='2' fill='#237847'/>"
+		"</svg>";
+
+	SDL_RWops *rw = SDL_RWFromConstMem(svgString.c_str(), svgString.size());
+	SDL_Surface *svgSurface = IMG_Load_RW(rw, 1);
+
+	struct SDL_Rect target { 50, 50, 100, 100 };
+
 	/* The event loop. */
 
 	SDL_Event event {};
@@ -49,15 +61,18 @@ int main()
 			if(event.type == SDL_KEYDOWN) {
 
 				switch(event.key.keysym.sym) {
-
 					case SDLK_ESCAPE:
 						running = false;
 						break;
-
 					default:
 						break;
-
 				}
+
+			} else if (event.type == SDL_MOUSEBUTTONDOWN) {
+
+            		if (event.button.button == SDL_BUTTON_LEFT) {
+                		SDL_GetMouseState(&target.x, &target.y);
+                	}
 
 			} else if(event.type == SDL_QUIT) {
 
@@ -68,6 +83,9 @@ int main()
 		}
 
 		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x06, 0x82, 0xBC));
+
+		SDL_BlitSurface( svgSurface, NULL, screenSurface, &target );
+
 		SDL_UpdateWindowSurface(window);
 
 		SDL_Delay(1000 / FPS);
